8b4962ac7c590f364519ba8e0dffd47d4ee45cad
blackbeans <blackbeans.zc@gmail.com>
2016-03-10 17:35:47 +0800

    migrate from kiteq 	new file:   benchmark/kite_benchmark_consumer.go 	new file:   benchmark/kite_benchmark_producer.go 	new file:   client/core/kite_client.go 	new file:   client/core/kite_client_handshake.go 	new file:   client/core/kite_client_manager.go 	new file:   client/core/kite_client_manager_test.go 	new file:   client/core/kite_zk_watcher.go 	new file:   client/handler/accept_event_handler.go 	new file:   client/handler/heartbeat_handler.go 	new file:   client/handler/packet_event_handler.go 	new file:   client/kiteq_client.go 	new file:   client/listener/kite_message_listener.go 	new file:   log/log_consumer.xml 	new file:   log/log_producer.xml

---
+++ client/kiteq_client.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"github.com/blackbeans/kiteq-client-go/client/core"
+	"github.com/blackbeans/kiteq-client-go/client/listener"
+	"github.com/blackbeans/kiteq-common/binding"
+	"github.com/blackbeans/kiteq-common/protocol"
+)
+
+type KiteQClient struct {
+	kclientManager *core.KiteClientManager
+}
+
+func (self *KiteQClient) Start() {
+	self.kclientManager.Start()
+}
+
+func NewKiteQClient(zkAddr, groupId, secretKey string, listener listener.IListener) *KiteQClient {
+	return &KiteQClient{
+		kclientManager: core.NewKiteClientManager(zkAddr, groupId, secretKey, listener)}
+}
+
+func (self *KiteQClient) SetTopics(topics []string) {
+	self.kclientManager.SetPublishTopics(topics)
+}
+
+func (self *KiteQClient) SetBindings(bindings []*binding.Binding) {
+	self.kclientManager.SetBindings(bindings)
+
+}
+
+func (self *KiteQClient) SendTxStringMessage(msg *protocol.StringMessage, transcation core.DoTranscation) error {
+	message := protocol.NewQMessage(msg)
+	return self.kclientManager.SendTxMessage(message, transcation)
+}
+
+func (self *KiteQClient) SendTxBytesMessage(msg *protocol.BytesMessage, transcation core.DoTranscation) error {
+	message := protocol.NewQMessage(msg)
+	return self.kclientManager.SendTxMessage(message, transcation)
+}
+
+func (self *KiteQClient) SendStringMessage(msg *protocol.StringMessage) error {
+	message := protocol.NewQMessage(msg)
+	return self.kclientManager.SendMessage(message)
+}
+
+func (self *KiteQClient) SendBytesMessage(msg *protocol.BytesMessage) error {
+	message := protocol.NewQMessage(msg)
+	return self.kclientManager.SendMessage(message)
+}
+
+func (self *KiteQClient) Destory() {
+	self.kclientManager.Destory()
+}
